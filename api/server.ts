9874e363e425405ec64141aa17bc0e94bd1ac10d
Lucca Felipetto <luccafelipetto@gmail.com>
2025-12-19 14:18:20 -0300

    developement and prodcution server better configurated

--- api/server.ts
+++ api/server.ts
@@ -1,26 +1,8 @@
-// ============================================
-// server script, where the server is opened
-// ============================================
+// ===================================================================
+// production server script, where the server is opened (production)
+// ===================================================================
 
-import 'dotenv/config';
 import app from "../src/app/app.ts";
-import { config } from 'dotenv';
-
-//load .env
-config({ override: true});
-
-//get the enviroment by the enviroment variable
-//can be developement or production
-const enviroment = process.env.NODE_ENV || "developement";
-
-//if the enviroment is developement, the server start listen at localhost
-if (enviroment == "developement") {
-    const PORT = 3000;
-
-    app.listen(PORT, () => {
-        console.log(`Server at http://localhost:${PORT}`);
-    });
-}
 
 //if the enviroment is production, we need to export a handler
 //because I deploy at vercel and it allows only one servelles function
---
+++ src/dev.ts
@@ -0,0 +1,12 @@
+// ===========================
+// dev.ts, the developement server
+// ===========================
+
+import app from "./app/app.ts";
+
+const PORT = 3000;
+
+//if the enviroment is developement, the server start listen at localhost
+app.listen(PORT, () => {
+    console.log(`Server at http://localhost:${PORT}`);
+});
